b50393455f0234a58ba954281f889312e67beac4
xNoobyyy <71840441+xNoobyyy@users.noreply.github.com>
2024-05-17 21:46:20 +0200

    chore: Update session data retrieval and update logic in action.ts

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -3,13 +3,17 @@ import { deleteCookie, setCookie, useSession } from "vinxi/http"
 
 export const actionDarkMode = action(async (darkMode: boolean) => {
   "use server"
-  setCookie("darkMode", darkMode ? "true" : "false")
+  setCookie("darkMode", darkMode ? "true" : "false", {
+    maxAge: 60 * 60 * 24 * 365,
+  })
 })
 
 export const actionSelected = action(async (value: string | undefined) => {
   "use server"
   if (value) {
-    setCookie("selected", value)
+    setCookie("selected", value, {
+      maxAge: 60 * 60 * 24 * 365,
+    })
   } else {
     deleteCookie("selected")
   }
