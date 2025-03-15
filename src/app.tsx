f5ae50089d32d77ab39dc61c0fc97047c51641bd
Leander Riefel <leanderriefel@gmx.de>
2025-03-15 08:54:25 +0100

    add analytics

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,11 @@ import { FileRoutes } from "@solidjs/start/router"
 import { Suspense } from "solid-js";
 import "./app.css"
 import { MetaProvider } from "@solidjs/meta"
+import { inject } from "@vercel/analytics"
  
 export default function App() {
+  inject()
+
   return (
     <Router
       root={props => (
